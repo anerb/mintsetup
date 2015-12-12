3b3527105f337e7ed8770f4f431652e897455c55
Aner Ben-Artzi <anerbenartzi@gmail.com>
2015-12-12 07:17:31 +0100

    added the shell script with some apt-get install/remove commands

---
+++ mintsetup.sh
@@ -0,0 +1,7 @@
+# sudo apt-get install git
+sudo apt-get install emacs
+sudo apt-get install gparted
+sudo apt-get remove --purge libreoffice*
+sudo apt-get remove --purge thunderbird*
+sudo apt-get remove --purge pidgin*
+sudo apt-get remove --purge tomboy*
