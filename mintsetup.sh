363ba8c20ab08bb92a93e49d0c6f331cfd5de70b
Aner Ben-Artzi <anerbenartzi@gmail.com>
2016-05-19 08:12:30 +0200

    add a user and stop screensaver

--- mintsetup.sh
+++ mintsetup.sh
@@ -1,4 +1,5 @@
 # sudo apt-get install git
+git config --global push.default simple
 sudo apt-get install emacs
 sudo apt-get install gparted
 sudo apt-get update --fix-missing
@@ -11,6 +12,18 @@ sudo apt-get remove --purge pidgin*
 sudo apt-get remove --purge tomboy*
 sudo apt-get install dosbox
 sudo apt-get install virualbox-5.0
+sudo apt-get install gimp
+sudo apt-get install blender
+
+# Google Chrome
 sudo sh -c 'echo "deb http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list'
 sudo apt-get update
 sudo apt-get install google-chrome-stable
+
+sudo addgroup guarded
+sudo useradd family -p `openssl passwd happy` -m -g guarded
+
+gsettings set org.cinnamon.settings-daemon.plugins.power lock-on-suspend 'false'
+gsettings set org.cinnamon.desktop.screensaver lock-enabled 'false'
+# GIMP: View > Show Layer Boundary
+# GIMP: View > Preferences > min/max undo raise the values
